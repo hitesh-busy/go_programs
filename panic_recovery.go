1a8951fec90291abc7b9697fe5a58b9e8114ef7a
abhishek-bhangalia-busy <158018556+abhishek-bhangalia-busy@users.noreply.github.com>
2024-02-26 19:48:23 +0530

    Create panic_recovery.go

---
+++ panic_recovery.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func divideByZero(a, b float64)(res float64,err error){
+	defer func(){
+		//Executing a call to recover inside a deferred function (but not any function called by it) stops the panicking sequence by restoring normal execution and retrieves the error value passed to the call of panic.
+		r := recover();
+		if r != nil {
+			err = fmt.Errorf("recovered from panic: %v", r)
+		}
+	}()
+
+	if (b == 0){
+		panic("division by zero")
+	}
+	return a/b, nil
+}
+
+
+func main(){
+	fmt.Printf("\n\nThis program divides two numbers\n")
+	for {
+		var a,b float64
+		var c string
+		fmt.Printf("\nEnter c to continue or q to quit : ")
+		fmt.Scanln(&c)
+		if c == "q" {
+			fmt.Println("Quiting...")
+			break
+		}
+
+		fmt.Printf("\nEnter first no : ")
+		fmt.Scanln(&a)
+		fmt.Printf("Enter second no : ")
+		fmt.Scanln(&b)
+		
+		res, err := divideByZero(a,b)
+		if err != nil {
+			fmt.Printf("Error : %v\n", err.Error())
+		} else{
+			fmt.Printf("%f / %f = %f\n", a, b, res)
+		}
+	}
+}
