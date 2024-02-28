0219dc128fb6e2985ec0813f2d1943802962e998
abhishek-bhangalia-busy <158018556+abhishek-bhangalia-busy@users.noreply.github.com>
2024-02-28 11:50:36 +0530

    Create fetch_json.go

---
+++ fetch_json.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+	"strconv"
+)
+
+type Result struct {
+	TotalPages int `json:"total_pages"`
+}
+
+func main() {
+
+	url := "https://jsonmock.hackerrank.com/api/weather/search?"
+	var name string
+	fmt.Printf("\nEnter name : ")
+	fmt.Scanln(&name)
+
+	url = url + "name=" + name
+	fmt.Println(url)
+
+	// Make a GET request to the endpoint
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := io.ReadAll(res.Body)
+	if readErr != nil {
+		fmt.Println(readErr)
+	}
+
+	result := Result{}
+	jsonErr := json.Unmarshal(body, &result)
+	if jsonErr != nil {
+		fmt.Println(jsonErr)
+	}
+
+	// fmt.Println(result)
+	var finalOut []interface{}
+	for i := 1; i <= result.TotalPages; i++ {
+		new_url := url + fmt.Sprintf("&page=%v", i)
+		fmt.Println(new_url)
+
+		// Make a GET request to the endpoint
+		res, err := http.Get(new_url)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		defer res.Body.Close()
+		// Read the response body
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println("Error reading response body:", err)
+			return
+		}
+
+		var data struct {
+			Data []struct {
+				Name    string   `json:"name"`
+				Weather string   `json:"weather"`
+				Status  []string `json:"status"`
+			} `json:"data"`
+		}
+
+		jsonErr := json.Unmarshal(body, &data)
+		if jsonErr != nil {
+			fmt.Println(jsonErr)
+		}
+
+		for _, v := range data.Data {
+			var out []interface{}
+			out = append(out, v.Name)
+			pattern := "[0-9]+"
+
+			re := regexp.MustCompile(pattern)
+			match := re.FindString(v.Weather)
+
+			if match != "" {
+				intValue, err := strconv.Atoi(match)
+				if err != nil {
+					fmt.Println("Error:", err)
+				} else {
+					fmt.Println("Integer value:", intValue)
+				}
+				out = append(out, intValue)
+			}
+			for _, val := range v.Status {
+				match := re.FindString(val)
+
+				if match != "" {
+					intValue, err := strconv.Atoi(match)
+					if err != nil {
+						fmt.Println("Error:", err)
+					} else {
+						fmt.Println("Integer value:", intValue)
+					}
+					out = append(out, intValue)
+				}
+
+			}
+			finalOut = append(finalOut, out)
+		}
+	}
+	fmt.Println(finalOut)
+}
